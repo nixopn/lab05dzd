2f6e18611512f5950fbafe7cd797438b37555f96
nixopn <144889708+nixopn@users.noreply.github.com>
2024-05-09 15:21:27 +0300

    Update tests.cpp

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -18,27 +18,27 @@ public:
 };
 
 TEST(Account, Mock) {
-	AccountMock acc(1, 100);
+	AccountMock acc(1, 200);
 	EXPECT_CALL(acc, GetBalance()).Times(1);
 	EXPECT_CALL(acc, ChangeBalance(testing::_)).Times(2);
 	EXPECT_CALL(acc, Lock()).Times(2);
 	EXPECT_CALL(acc, Unlock()).Times(1);
 	acc.GetBalance();
-	acc.ChangeBalance(100); // throw
+	acc.ChangeBalance(200); // throw
 	acc.Lock();
-	acc.ChangeBalance(100);
+	acc.ChangeBalance(200);
 	acc.Lock(); // throw
 	acc.Unlock();
 }
 
 TEST(Account, SimpleTest) {
-	Account acc(1, 100);
-	EXPECT_EQ(acc.id(), 1);
-	EXPECT_EQ(acc.GetBalance(), 100);
-	EXPECT_THROW(acc.ChangeBalance(100), std::runtime_error);
-	EXPECT_NO_THROW(acc.Lock());
-	acc.ChangeBalance(100);
+	Account acc(2, 200);
+	EXPECT_EQ(acc.id(), 2);
 	EXPECT_EQ(acc.GetBalance(), 200);
+	EXPECT_THROW(acc.ChangeBalance(200), std::runtime_error);
+	EXPECT_NO_THROW(acc.Lock());
+	acc.ChangeBalance(200);
+	EXPECT_EQ(acc.GetBalance(), 400);
 	EXPECT_THROW(acc.Lock(), std::runtime_error);
 }
 
